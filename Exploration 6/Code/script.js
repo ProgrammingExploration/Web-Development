49fa85ca52d1a91ac8379e1c030f28c9466304b3
littlepotato123 <sopanda0910@gmail.com>
2020-11-13 15:38:36 -0500

    Exploration 6 - Done

---
+++ Exploration 6/Code/script.js
@@ -0,0 +1,7 @@
+const text = document.getElementById('text');
+text.innerText = prompt('What do you want to display');
+console.log(confirm('Are you sure you want to do this'));
+/*
+    OK -> True
+    Cancel -> False
+*/
