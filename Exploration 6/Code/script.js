cbe5f7e33a0590be2b07fd637db8485b922d7a91
littlepotato123 <sopanda0910@gmail.com>
2020-11-16 08:07:12 -0500

    Exploration 7 - Setup

--- Exploration 6/Code/script.js
+++ Exploration 6/Code/script.js
@@ -5,3 +5,23 @@ console.log(confirm('Are you sure you want to do this'));
     OK -> True
     Cancel -> False
 */
+
+const blue = document.getElementById('blue');
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+
+let width, height;
+window.onresize = () => {
+    width = this.innerWidth;
+    height = this.innerHeight;
+    // Checks
+    if(width <= 600) {
+        blue.innerText = 'Red';
+        red.innerText = 'Green';
+        green.innerText = 'Blue';
+    } else {
+        blue.innerText = 'Blue';
+        red.innerText = 'Red';
+        green.innerText = 'Green';
+    }
+}
